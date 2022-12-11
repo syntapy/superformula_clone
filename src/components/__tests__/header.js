c34112ba61a6b6a7b585404f5ae7ef0ee0363718
Sami Abdul-Wahid <syntapy@pm.me>
2022-12-10 20:23:40 -0800

    add tests for <A /> and <NavBar /> tags

--- src/components/__tests__/header.js
+++ src/components/__tests__/header.js
@@ -2,6 +2,8 @@ import React from "react"
 import renderer from "react-test-renderer"
 
 import Header from "../header"
+import { A } from "../buttons"
+import NavBar from "../navbar"
 
 jest.mock("gatsby")
 
@@ -9,6 +11,24 @@ describe("Header", () => {
 	it("renders correctly", () => {
 		const tree = renderer
 			.create(<Header siteTitle="Default Starter" />)
+			.toJSON()
+		expect(tree).toMatchSnapshot()
+	})
+})
+
+describe("A", () => {
+	it("renders correctly", () => {
+		const tree = renderer
+			.create(<A href={"test"} text={"test url"}>Test Url</A>)
+			.toJSON()
+		expect(tree).toMatchSnapshot()
+	})
+})
+
+describe("NavBar", () => {
+	it("renders correctly", () => {
+		const tree = renderer
+			.create(<NavBar />)
 			.toJSON()
 		expect(tree).toMatchSnapshot()
 	})
