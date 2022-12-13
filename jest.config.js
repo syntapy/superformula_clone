7d1d712bb281820b9bd37f1bf3c3a5335a1f647c
Sami Abdul-Wahid <syntapy@pm.me>
2022-12-13 10:55:05 -0800

    can import css classes into tests

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
     "^.+\\.[jt]sx?$": `<rootDir>/jest-preprocess.js`,
   },
   moduleNameMapper: {
-    ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
+    ".+\\.(css|styl|less|sass|scss)$": `<rootDir>/identity-obj-proxy-css.js`,
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/file-mock.ts",
     ".+\\.svg": "<rootDir>/__mocks__/svg.js"
   },
