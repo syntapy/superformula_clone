82bf33d09230ea50845cdaece724ffd59178ab59
Sami Abdul-Wahid <syntapy@pm.me>
2022-12-13 11:05:15 -0800

    pass homeItem class to HomeNavButton

--- src/components/__tests__/screenshot.js
+++ src/components/__tests__/screenshot.js
@@ -6,11 +6,12 @@ import '@testing-library/jest-dom'
 
 import * as React from 'react'
 import {render, fireEvent, screen} from '@testing-library/react'
+import * as styles from "../../styles/navbar.module.css"
 import { HomeNavButton } from "../buttons"
 
 test('test HomeNavButton', () => {
 	const testMessage = 'Test Message'
-	render(<HomeNavButton href={"/test"} text={testMessage} />)
+	render(<HomeNavButton href={"/test"} text={testMessage} className={styles.homeItem}/>)
 
 	const buttonClass = HomeNavButton.name
 	console.log(HomeNavButton())
