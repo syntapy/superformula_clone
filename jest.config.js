ac6c623747e737440f44f9d41289251367cd73a3
Sami Abdul-Wahid <syntapy@pm.me>
2022-12-14 18:20:11 -0800

    try jest-transform-css
    it might work to test actuall css styles!

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   transform: {
     "^.+\\.[jt]sx?$": `<rootDir>/jest-preprocess.js`,
-    "^.+\\.module\\.(css)$": `jest-css-modules-transform`,
+    "^.+\\.(css)$": [`jest-transform-css`, { 
+      modules: true,
+      generateScopedName: '[path]_[name]_[local]'
+    }],
   },
   moduleNameMapper: {
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/file-mock.ts",
