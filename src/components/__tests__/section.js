1c820109c83905471531c29e5dc589ece973de1d
Sami Abdul-Wahid <syntapy@pm.me>
2022-12-19 22:08:26 -0800

    add culture section test setup

--- src/components/__tests__/section.js
+++ src/components/__tests__/section.js
@@ -3,8 +3,14 @@ import '@testing-library/jest-dom'
 import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import * as utilStyles from '../../styles/utils.module.css'
-import { ServicesSection } from '../section'
+import { ServicesSection, CultureSection } from '../section'
 import img_c from '../images/img_c.png'
+
+import team1 from "../images/team/team1.png"
+import team2 from "../images/team/team2.png"
+import team3 from "../images/team/team3.png"
+import team4 from "../images/team/team4.png"
+import team_img from "../images/team/team_img.png"
 
 describe('services section', () => {
 
@@ -45,4 +51,36 @@ describe('services section', () => {
 		expect(imgStyle).toHaveProperty('margin', '0px auto')
 		expect(imgStyle).toHaveProperty('padding', '0px 1rem')
 	})
+})
+
+const isMobileFalse = jest.fn(() => false)
+const isMobileTrue = jest.fn(() => true)
+
+describe('culture section', () => {
+
+	let h3
+	let h5
+	let p
+
+	beforeEach(() => {
+		const { container } = render(
+          <CultureSection
+            title="100% remote and loving it"
+            subtitle="Collaborative, diverse, and really, really good at what we do"
+            description="Because we’re a team of passionate problem solvers doing work we love in a way that works, in every respect. In other words, low-ego, high-drive—and it makes all the difference"
+            href="https://careers.superformula.com/culture-values" text="Explore our culture"
+            imgListDesktop={[
+              {src: "./team1", alt: "Team 1"},
+              {src: "./team2", alt: "Team 2"},
+              {src: "./team3", alt: "Team 3"},
+              {src: "./team4", alt: "Team 4"},
+            ]}
+            imgMobile={{src: team_img, alt: "Team"}}
+            isMobile={isMobileFalse}
+          />)
+	})
+
+	it('culture section', () => {
+	})
+
 })
