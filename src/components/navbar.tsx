61c9e350f200ebb2546fcb4abd693010ea653ec4
Sami Abdul-Wahid <syntapy@pm.me>
2022-12-21 18:18:17 -0800

    fixed navbar location

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,24 +7,28 @@ import * as utilStyles from "../styles/utils.module.css"
 import Chilidogmenu from "../images/svg/chilidogmenu.svg"
 
 export default function NavBar() {
+  const navBarPadding: string = styles.navbarContainer
   const navBar: string = styles.container + " " + styles.navbar
   const homeItem: string = styles.homeItem
   const pageItemList: string = styles.navItem + " " + styles.container
   const pageItem: string = styles.pageItem + " " + styles.navItem
+  const chilidogStyle: string = styles.chilidog
   function onChilidogClick() {
     console.log("Chilidog clicked")
   }
   return (
-    <div className={navBar}>
-      <Chilidogmenu onClick={onChilidogClick} className={styles.chilidog} />
-      <HomeNavButton className={homeItem} href="/" text="Superformula." />
-      <nav className={pageItemList}>
-        <NavButton className={pageItem} href="/services" text="Services" />
-        <NavButton className={pageItem} href="/work" text="Work" />
-        <NavButton className={pageItem} href="/articles" text="Articles" />
-        <NavButton className={pageItem} href="/contact" text="Contact" />
-        <CareersNavButton className={pageItem} href="https://careers.superformula.com" text="Careers" />
-      </nav>
+    <div className={navBarPadding}>
+      <div className={navBar}>
+        <Chilidogmenu onClick={onChilidogClick} className={chilidogStyle} />
+        <HomeNavButton className={homeItem} href="/" text="Superformula." />
+        <nav className={pageItemList}>
+          <NavButton className={pageItem} href="/services" text="Services" />
+          <NavButton className={pageItem} href="/work" text="Work" />
+          <NavButton className={pageItem} href="/articles" text="Articles" />
+          <NavButton className={pageItem} href="/contact" text="Contact" />
+          <CareersNavButton className={pageItem} href="https://careers.superformula.com" text="Careers" />
+        </nav>
+      </div>
     </div>
   )
 }
