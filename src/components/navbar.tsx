9fcde2b32dc508ac377b44a1ed4cd62d9d458b28
Sami Abdul-Wahid <syntapy@pm.me>
2022-12-23 09:56:20 -0800

    remove left padding with separate NavBarItems component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,31 +6,46 @@ import * as utilStyles from "../styles/utils.module.css"
 
 import Chilidogmenu from "../images/svg/chilidogmenu.svg"
 
+interface NavbarItemsProps {
+  listClassName: string
+  homeClassName: string
+  itemClassName: string
+}
+
+function NavBarItems(props: NavbarItemsProps) {
+  return (
+    <nav className={props.listClassName}>
+      <HomeNavButton className={props.homeClassName} href="/" text="Superformula." />
+      <NavButton className={props.itemClassName} href="/services" text="Services" />
+      <NavButton className={props.itemClassName} href="/work" text="Work" />
+      <NavButton className={props.itemClassName} href="/articles" text="Articles" />
+      <NavButton className={props.itemClassName} href="/contact" text="Contact" />
+      <CareersNavButton className={props.itemClassName} href="https://careers.superformula.com" text="Careers" />
+    </nav>
+  )
+}
+
 export default function NavBar() {
   const navBarPadding: string = styles.navbarContainer
   const navBar: string = styles.container + " " + styles.navbar
   const homeItem: string = styles.homeItem
   const pageItemList: string = styles.navItem + " " + styles.pageItemList
+  const pageItemListMobile: string = styles.navItem + " " + styles.pageItemListMobile
   const pageItem: string = styles.pageItem + " " + styles.navItem
-  const mobileHomeItem: string = styles.pageItem + " " + styles.mobileHomeItem + " " + styles.navItem
   const chilidogStyle: string = styles.chilidog
   function onChilidogClick() {
     console.log("Chilidog clicked")
   }
   return (
-    <div className={navBarPadding}>
-      <div className={navBar}>
-        <Chilidogmenu onClick={onChilidogClick} className={chilidogStyle} />
-        <HomeNavButton className={homeItem} href="/" text="Superformula." />
-        <nav className={pageItemList}>
-          <HomeNavButton className={mobileHomeItem} href="/" text="Superformula." />
-          <NavButton className={pageItem} href="/services" text="Services" />
-          <NavButton className={pageItem} href="/work" text="Work" />
-          <NavButton className={pageItem} href="/articles" text="Articles" />
-          <NavButton className={pageItem} href="/contact" text="Contact" />
-          <CareersNavButton className={pageItem} href="https://careers.superformula.com" text="Careers" />
-        </nav>
+    <>
+      <div className={navBarPadding}>
+        <div className={navBar}>
+          <Chilidogmenu onClick={onChilidogClick} className={chilidogStyle} />
+          <HomeNavButton className={homeItem} href="/" text="Superformula." />
+          <NavBarItems listClassName={pageItemList} homeClassName={homeItem} itemClassName={styles.navItem} />
+        </div>
       </div>
-    </div>
+      <NavBarItems listClassName={pageItemListMobile} homeClassName={homeItem} itemClassName={pageItem} />
+    </>
   )
 }
