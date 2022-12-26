fd86eea88a0ed882371105757f6f3d61a67ce988
Sami Abdul-Wahid <syntapy@pm.me>
2022-12-26 12:48:53 -0800

    use a class component in __mocks__/svg.tsx

--- __mocks__/svg.tsx
+++ __mocks__/svg.tsx
@@ -1,10 +1,25 @@
 //export default 'SvgrURL'
+import * as React from 'react'
 
-export default function SvgrURL() {
-  return <svg viewBox="0 0 80 50" width="32" height="45" fill="#ffffff">
-    <rect width="44" height="5"></rect>
-    <rect y="20" width="80" height="5"></rect>
-    <rect y="40" width="50" height="5"></rect>
-  </svg>
+console.log("MOCKING SVG")
+export default class SvgrURL extends React.Component<{}, {}> {
+  constructor(props: {}) {
+    super(props)
+    console.log('SvgrURL')
+  }
+
+  componentDidMount() {
+    console.log("Did mount")
+  }
+
+  render() {
+    console.log("RENDER")
+    return <svg viewBox="0 0 80 50" width="32" height="45" fill="#ffffff">
+      <rect width="44" height="5"></rect>
+      <rect y="20" width="80" height="5"></rect>
+      <rect y="40" width="50" height="5"></rect>
+    </svg>
+  }
 }
+
 export const ReactComponent = 'svg'
