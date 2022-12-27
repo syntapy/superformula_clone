289bfaa02cee50cb1b5d0ac6b00547e12cb329d1
Sami Abdul-Wahid <syntapy@pm.me>
2022-12-27 00:14:48 -0800

    add css tests but media query aspect fails

--- src/components/__tests__/navbar.spec.js
+++ src/components/__tests__/navbar.spec.js
@@ -5,12 +5,76 @@ import {render, screen} from '@testing-library/react'
 
 import NavBar from '../navbar'
 
-describe('menu', () => {
+import {setMediaQueryMobile, setMediaQueryDesktop} from './utils'
+
+const isMobileFalse = jest.fn(() => false)
+const isMobileTrue = jest.fn(() => true)
+
+describe('desktop navbar', () => {
+	let desktopNavBar
+	let mobileNavBar
+
 	beforeEach(() => {
-		const { container } = render(<NavBar />)
+		setMediaQueryDesktop(window)
+		console.log(window.matchMedia('(max-width: 678px)').matches)
+		const rendered = render(<NavBar isMobile={isMobileFalse} />)
+		const container = rendered.container
+		desktopNavBar = container.querySelector('#desktop-nav-wrapper')
+		mobileNavBar = container.querySelector('#mobile-nav')
 	})
 
-	it('should render the navbar', () => {
-		//expect(screen.getById('desktop-nav')).toBeInTheDocument()
+	it('desktop navbar exists', () => {
+		expect(desktopNavBar).toBeInTheDocument()
+	})
+
+	it('mobile navbar exists', () => {
+		expect(mobileNavBar).toBeInTheDocument()
+	})
+
+	it('desktop navbar IS visible', () => {
+		const style = window.getComputedStyle(desktopNavBar)
+		expect(style.display).toBe('block')
+		expect(desktopNavBar).toBeVisible()
+	})
+
+	it('mobile navbar is NOT visible', () => {
+		const style = window.getComputedStyle(mobileNavBar)
+		expect(style.display).toBe('none')
+		expect(mobileNavBar).not.toBeVisible()
+	})
+})
+
+describe('mobile navbar', () => {
+	let desktopNavBar
+	let mobileNavBar
+
+	beforeEach(() => {
+		setMediaQueryMobile(window)
+		const rendered = render(<NavBar isMobile={isMobileTrue} />)
+		const container = rendered.container
+		desktopNavBar = container.querySelector('#desktop-nav-wrapper')
+		mobileNavBar = container.querySelector('#mobile-nav')
+	})
+
+	it('desktop navbar exists', () => {
+		expect(desktopNavBar).toBeInTheDocument()
+	})
+
+	it('mobile navbar exists', () => {
+		expect(mobileNavBar).toBeInTheDocument()
+	})
+
+	it('desktop navbar is NOT visible', () => {
+		const style = window.getComputedStyle(desktopNavBar)
+		expect(style.display).toBe('none')
+		expect(desktopNavBar).not.toBeVisible()
+	})
+
+	it('mobile navbar is NOT visible before click', () => {
+		const style = window.getComputedStyle(mobileNavBar)
+		expect(style.display).toBe('flex')
+		expect(style.visibility).toBe('hidden')
+		expect(style.bottom).toBe('100%')
+		expect(mobileNavBar).not.toBeVisible()
 	})
 })
