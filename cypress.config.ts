9502a18dc2eca8fa203be1e647f7f1b036057e8d
Sami Abdul-Wahid <syntapy@pm.me>
2023-01-02 19:55:15 -0800

    disable video capture

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    video: false,
     baseUrl: "http://localhost:8000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
