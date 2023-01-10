8a8ffbb93970631ef6ecbc432f514f881e053021
Sami Abdul-Wahid <syntapy@pm.me>
2023-01-09 21:29:10 -0800

    move cy.visit from beforeAll to test

--- cypress/e2e/desktop.cy.js
+++ cypress/e2e/desktop.cy.js
@@ -17,7 +17,6 @@ describe('example to-do app', () => {
     // so we must tell it to visit our website with the `cy.visit()` command.
     // Since we want to visit the same URL at the start of all our tests,
     // we include it in our beforeEach function so that it runs before each test
-    cy.visit('/')
     cy.eyesOpen({
       appName: 'Superformula Clone',
       testName: Cypress.currentTest.title
@@ -25,6 +24,7 @@ describe('example to-do app', () => {
   })
 
   it('top screenshot', () => {
+    cy.visit('/')
     cy.eyesCheckWindow({
       tag: 'Top',
       target: 'window',
