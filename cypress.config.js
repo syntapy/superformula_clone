53804af7116e7ca446664c6f5f1151cf5ce92fa5
Sami Abdul-Wahid <syntapy@pm.me>
2023-01-11 09:10:21 -0800

    use gatsby serve instead of develop

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   video: false,
   
   e2e: {
-    baseUrl: "http://localhost:8000",
+    baseUrl: "http://localhost:9000",
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
