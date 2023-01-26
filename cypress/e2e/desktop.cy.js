567d61288c838702b9d3e5afc98bea6f1f468883
Sami Abdul-Wahid <syntapy@pm.me>
2023-01-25 22:17:43 -0800

    re-enable all tests except snapshot comparisons

--- cypress/e2e/desktop.cy.js
+++ cypress/e2e/desktop.cy.js
@@ -16,18 +16,18 @@ desktop.devices.forEach((device) => {
         sections.should('have.length', 6)
 
         // get 2nd section
-        //const div = sections.eq(1).get('div')
-        //const img_left = div.eq(0).get('img').eq(0)
-        //const img_right = div.eq(0).get('img').eq(1)
+        const div = sections.eq(1).get('div')
+        const img_left = div.eq(0).get('img').eq(0)
+        const img_right = div.eq(0).get('img').eq(1)
 
-        // without this sometimes they don't show up in the screenshot
-        //img_left.should('be.visible')
-        //img_right.should('be.visible')
+         without this sometimes they don't show up in the screenshot
+        img_left.should('be.visible')
+        img_right.should('be.visible')
         
-        // wait for rendering to finish
-        //cy.wait(waitTime)
+         wait for rendering to finish
+        cy.wait(waitTime)
 
-        //const fname = 'page_desktop_' + device + '_' + orientation
+        const fname = 'page_desktop_' + device + '_' + orientation
         //cy.compareSnapshot(fname)
       })
     })
