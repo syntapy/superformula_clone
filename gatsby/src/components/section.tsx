1af7f65badce77825cfb5c496991b5c0ef29897a
Sami Abdul-Wahid <syntapy@pm.me>
2023-02-13 17:56:15 -0800

    section.module.css -> section/index.module.css

--- gatsby/src/components/section.tsx
+++ gatsby/src/components/section.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import * as styles from "../styles/section.module.css"
+import * as styles from "../styles/section/index.module.css"
 import * as servicesStyles from "../styles/section/services.module.css"
 import * as utilStyles from "../styles/utils.module.css"
 import { mobileV_desktopH } from "../styles/utilities/common"
