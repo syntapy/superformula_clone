f870c91a33486ffdf1dce86264d38d6f77020719
Sami Abdul-Wahid <syntapy@pm.me>
2024-06-03 18:59:10 -0700

    got close button to look ok, but code is crappy

--- gatsby/src/components/toast.tsx
+++ gatsby/src/components/toast.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import * as styles from "../styles/toast.module.css"
 import * as orientationStyles from "../styles/utilities/orientation.module.css"
+import * as otherStyles from "../styles/utilities/orientation.module.css"
 import Check from "../images/svg/check.svg"
 import Exclamation from "../images/svg/exclamation.svg"
 import Close from "../images/svg/close.svg"
@@ -15,11 +16,15 @@ export default function Toast(props: ToastProps) {
   const insetShadowStyle: string = styles.insetShadow
   const toastStyle: string = styles.toast
   const horizontalFlex: string = orientationStyles.horizontalFlex
+  const flexPadStyle: string = styles.flexPad
   const btnClassName: string = styles.toastOkButton
   const exclamationStyle: string = styles.exclamation
   const closeStyle: string = styles.close
-  const closeParStyle: string = styles.closePar
-  const padStyle: string = styles.pad
+  const padCloseStyle: string = styles.padClose
+  const borderStyle: string = styles.border
+  const centerStyle: string = orientationStyles.flexStartItems
+  const pointerStyle: string = otherStyles.pointer
+  const inlineStyle: string = otherStyles.inline
 
   function onCloseClick(): void {
 
@@ -29,12 +34,12 @@ export default function Toast(props: ToastProps) {
     <div className={toastContainerStyle}>
     <div className={insetShadowStyle}>
     <div className={toastStyle}>
-    <div>
-      <div className={padStyle}>
+    <div className={horizontalFlex + " " + centerStyle + " " + flexPadStyle}>
+      <SvgButton className={inlineStyle} icon={<Exclamation width={14} height={14} data-cy="toast-exclamation-svg" />} />
+      <div className={borderStyle}>
+        {props.message}
       </div>
-      <SvgButton icon={<Exclamation width={12} height={12} data-cy="toast-exclamation-svg" />} />
-      {props.message}
-      <button className={closeStyle}>x</button>
+      <button className={closeStyle + " " + padCloseStyle + " " + pointerStyle}>x</button>
     </div>
     </div>
     </div>
