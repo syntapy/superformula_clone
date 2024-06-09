5c8626dbfbd76e02bc2481e29da9fedab9d8c474
Sami Abdul-Wahid <syntapy@pm.me>
2024-06-09 13:55:01 -0700

    remove unused mobile.tsx & desktop.tsx files

--- gatsby/src/components/navbar/desktop.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react"
-import * as styles from "../../styles/navbar.module.css"
-import * as orientationStyles from "../../styles/utilities/orientation.module.css"
-import { desktop } from "../../styles/utilities/visibility.module.css"
-import { HomeNavButton, NavButton, CareersNavButton, SvgButton } from "../buttons"
-import { graphql, useStaticQuery, HeadFC } from "gatsby"
-
-import { NavProps, NavItems, NavItemsList } from "./common"
