18d93cef28e8dc02ac07531d6871aedad0089ebd
Sami Abdul-Wahid <syntapy@pm.me>
2024-06-10 19:34:45 -0700

    add padding / margin around header

--- gatsby/src/components/header.tsx
+++ gatsby/src/components/header.tsx
@@ -4,9 +4,12 @@ import type { HeadFC } from "gatsby"
 import NavBar from "../components/navbar"
 import Toast from "../components/toast"
 
+import * as styles from "../styles/header.module.css"
+
 export default function Header() {
+  const headerStyles: string = styles.header
   return (
-    <header>
+    <header className={headerStyles}>
       <NavBar />
       <Toast message={"This is a practice clone site and is not the actual Superformula website"} />
     </header>
