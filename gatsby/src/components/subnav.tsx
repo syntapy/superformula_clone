d0d813f8dfd3aa91ed0bae1f12b5f9a36fb5ac67
Sami Abdul-Wahid <syntapy@pm.me>
2024-08-19 16:45:28 -0700

    use flex gap for subNavItem spacing

--- gatsby/src/components/subnav.tsx
+++ gatsby/src/components/subnav.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import * as buttonStyles from "../styles/buttons.module.css"
+import * as orientationStyles from "../styles/utilities/orientation.module.css"
 import { useStaticQuery, graphql } from "gatsby"
 
 export type SubNavItem = {
@@ -14,8 +15,9 @@ export interface SubNavProps {
 
 export function SubNavBar(props: SubNavProps) {
   if (props.subNavItems !== undefined) {
+    const className = buttonStyles.subNav + " " + orientationStyles.verticalFlex + " " + orientationStyles.spaceBetweenItems
     return (
-      <div className={buttonStyles.subNav}>
+      <div className={className}>
         {props.subNavItems.map((item) => (
           <div className={buttonStyles.subNavItem}>{item.icon}{item.text}</div>
         ))}
