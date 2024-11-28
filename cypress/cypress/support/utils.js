98e0325d8246456e0cea1a70f6d6ba0ecfa17bf9
Sami Abdul-Wahid <syntapy@pm.me>
2024-11-27 16:26:48 -0800

    Revert "set always as desktop on mobile if landscape"
    This reverts commit 2371aa83d42f163354c0d5ad8a3c107c6a82e555.

--- cypress/cypress/support/utils.js
+++ cypress/cypress/support/utils.js
@@ -33,7 +33,7 @@ export const waitTime = 2400
 export const animationWaitTime = 750
 
 export function isDesktopMenuOnMobile(device, orientation) {
-  return orientation === 'landscape'
+  return orientation === 'landscape' && ['iphone-xr'].includes(device)
 }
 
 export const errorThreshold = 0.01
