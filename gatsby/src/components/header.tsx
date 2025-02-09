87095286d2039e0adadb8db932d06342fe5715ac
Sami Abdul-Wahid <syntapy@pm.me>
2025-02-08 20:07:45 -0800

    mv navbar.tsx -> nav/NavBar.tsx

--- gatsby/src/components/header.tsx
+++ gatsby/src/components/header.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import type { HeadFC } from "gatsby"
 
-import NavBar from "../components/navbar"
+import NavBar from "../components/nav/NavBar"
 import Toast from "../components/toast"
 
 import * as styles from "../styles/header.module.css"
