7e88b2e7aae385bb91268410112fdef352128cc7
Sami Abdul-Wahid <syntapy@pm.me>
2025-02-09 19:33:26 -0800

    remove unneeded CareersNavButton import

--- gatsby/src/components/nav/NavBar.tsx
+++ gatsby/src/components/nav/NavBar.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
-import { HomeNavButton, NavButton, NavButtonMobile, SvgButton, CareersNavButton } from "../button/index.ts"
+import { HomeNavButton, NavButton, NavButtonMobile, SvgButton } from "../button/index.ts"
 
 import NavItems from "./NavItems.tsx"
 
