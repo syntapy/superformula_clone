57a256ca49e27f3cac044ce4eb1ee5f68e6b955b
Sami Abdul-Wahid <syntapy@pm.me>
2025-03-25 14:43:09 -0700

    add leading slash to path prefix

--- gatsby/gatsby-config.ts
+++ gatsby/gatsby-config.ts
@@ -7,7 +7,7 @@ dotenv.config({
 });
 
 const config: GatsbyConfig = {
-  pathPrefix: "superformula_clone",
+  pathPrefix: "/superformula_clone",
   siteMetadata: {
     title: `Superformula LP Clone`,
     siteUrl: `https://www.yourdomain.tld`
