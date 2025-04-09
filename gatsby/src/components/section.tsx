f02bf4cfd8fe6fb423f3f3a396d870f680d5f7df
Sami Abdul-Wahid <syntapy@pm.me>
2025-04-09 16:21:21 -0700

    move SectionProps to separate types.ts

--- gatsby/src/components/section.tsx
+++ gatsby/src/components/section.tsx
@@ -8,13 +8,8 @@ import { Ol, Li } from "./info"
 
 import * as utils from "../utils"
 
-interface SectionProps {
-  children: JSX.Element
-}
-
-interface StyleProps {
-  className?: string
-}
+import { StyleProps } from "../../utils"
+import { SectionProps } from "./section/types.ts"
 
 export function SectionBlack(props: SectionProps & StyleProps) {
   let className: string = styles.section + " " + styles.sectionBlack + " " + props.className
