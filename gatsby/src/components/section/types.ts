b0dfdaf326d38c1bcca2a4ff8420b1915284d468
Sami Abdul-Wahid <syntapy@pm.me>
2025-04-10 13:35:29 -0700

    add missing export for HeaderSectionProps

--- gatsby/src/components/section/types.ts
+++ gatsby/src/components/section/types.ts
@@ -2,7 +2,7 @@ export interface SectionProps {
   children: JSX.Element
 }
 
-interface HeaderSectionProps {
+export interface HeaderSectionProps {
   title: string
   description: string
   href: string
